76ed5fad344f2e133f415211f2ec496d669475bf
mehmetari46 <83248135+mehmetari46@users.noreply.github.com>
2021-11-22 23:08:28 +0300

    Add files via upload

---
+++ ndpödev2soru2/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace ndpödev2soru2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matris13 = new System.Windows.Forms.TextBox();
+            this.matris21 = new System.Windows.Forms.TextBox();
+            this.matris23 = new System.Windows.Forms.TextBox();
+            this.matris24 = new System.Windows.Forms.TextBox();
+            this.matris22 = new System.Windows.Forms.TextBox();
+            this.matris11 = new System.Windows.Forms.TextBox();
+            this.matris14 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.görüntübtn = new System.Windows.Forms.Button();
+            this.sonuç1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sonuç5 = new System.Windows.Forms.Label();
+            this.sonuç3 = new System.Windows.Forms.Label();
+            this.sonuç2 = new System.Windows.Forms.Label();
+            this.sonuç4 = new System.Windows.Forms.Label();
+            this.izbtn = new System.Windows.Forms.Button();
+            this.transpozbtn = new System.Windows.Forms.Button();
+            this.tersibtn = new System.Windows.Forms.Button();
+            this.çarpbtn = new System.Windows.Forms.Button();
+            this.toplambtn = new System.Windows.Forms.Button();
+            this.matris12 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // matris13
+            // 
+            this.matris13.Location = new System.Drawing.Point(48, 118);
+            this.matris13.Name = "matris13";
+            this.matris13.Size = new System.Drawing.Size(37, 23);
+            this.matris13.TabIndex = 0;
+            // 
+            // matris21
+            // 
+            this.matris21.Location = new System.Drawing.Point(260, 67);
+            this.matris21.Name = "matris21";
+            this.matris21.Size = new System.Drawing.Size(37, 23);
+            this.matris21.TabIndex = 1;
+            // 
+            // matris23
+            // 
+            this.matris23.Location = new System.Drawing.Point(260, 118);
+            this.matris23.Name = "matris23";
+            this.matris23.Size = new System.Drawing.Size(37, 23);
+            this.matris23.TabIndex = 2;
+            // 
+            // matris24
+            // 
+            this.matris24.Location = new System.Drawing.Point(340, 118);
+            this.matris24.Name = "matris24";
+            this.matris24.Size = new System.Drawing.Size(37, 23);
+            this.matris24.TabIndex = 3;
+            // 
+            // matris22
+            // 
+            this.matris22.Location = new System.Drawing.Point(340, 67);
+            this.matris22.Name = "matris22";
+            this.matris22.Size = new System.Drawing.Size(37, 23);
+            this.matris22.TabIndex = 4;
+            // 
+            // matris11
+            // 
+            this.matris11.Location = new System.Drawing.Point(48, 67);
+            this.matris11.Name = "matris11";
+            this.matris11.Size = new System.Drawing.Size(37, 23);
+            this.matris11.TabIndex = 5;
+            // 
+            // matris14
+            // 
+            this.matris14.Location = new System.Drawing.Point(115, 118);
+            this.matris14.Name = "matris14";
+            this.matris14.Size = new System.Drawing.Size(37, 23);
+            this.matris14.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Yellow;
+            this.groupBox1.Controls.Add(this.görüntübtn);
+            this.groupBox1.Controls.Add(this.sonuç1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.sonuç5);
+            this.groupBox1.Controls.Add(this.sonuç3);
+            this.groupBox1.Controls.Add(this.sonuç2);
+            this.groupBox1.Controls.Add(this.sonuç4);
+            this.groupBox1.Controls.Add(this.izbtn);
+            this.groupBox1.Controls.Add(this.transpozbtn);
+            this.groupBox1.Controls.Add(this.tersibtn);
+            this.groupBox1.Controls.Add(this.çarpbtn);
+            this.groupBox1.Controls.Add(this.toplambtn);
+            this.groupBox1.Controls.Add(this.matris12);
+            this.groupBox1.Controls.Add(this.matris11);
+            this.groupBox1.Controls.Add(this.matris24);
+            this.groupBox1.Controls.Add(this.matris22);
+            this.groupBox1.Controls.Add(this.matris14);
+            this.groupBox1.Controls.Add(this.matris23);
+            this.groupBox1.Controls.Add(this.matris13);
+            this.groupBox1.Controls.Add(this.matris21);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(453, 421);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MATRİS HESAPLA";
+            // 
+            // görüntübtn
+            // 
+            this.görüntübtn.Location = new System.Drawing.Point(329, 338);
+            this.görüntübtn.Name = "görüntübtn";
+            this.görüntübtn.Size = new System.Drawing.Size(90, 23);
+            this.görüntübtn.TabIndex = 28;
+            this.görüntübtn.Text = "GÖRÜNTÜLE";
+            this.görüntübtn.UseVisualStyleBackColor = true;
+            this.görüntübtn.Click += new System.EventHandler(this.görüntübtn_Click);
+            // 
+            // sonuç1
+            // 
+            this.sonuç1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sonuç1.Location = new System.Drawing.Point(48, 321);
+            this.sonuç1.Name = "sonuç1";
+            this.sonuç1.Size = new System.Drawing.Size(37, 23);
+            this.sonuç1.TabIndex = 27;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Yellow;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(218, 287);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 21);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "SONUÇ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Yellow;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(76, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 21);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "SONUÇ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Yellow;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(278, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 21);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "MATRİS 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(61, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 21);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "MATRİS 1";
+            // 
+            // sonuç5
+            // 
+            this.sonuç5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sonuç5.Location = new System.Drawing.Point(227, 342);
+            this.sonuç5.Name = "sonuç5";
+            this.sonuç5.Size = new System.Drawing.Size(48, 32);
+            this.sonuç5.TabIndex = 22;
+            // 
+            // sonuç3
+            // 
+            this.sonuç3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sonuç3.Location = new System.Drawing.Point(48, 370);
+            this.sonuç3.Name = "sonuç3";
+            this.sonuç3.Size = new System.Drawing.Size(37, 23);
+            this.sonuç3.TabIndex = 21;
+            // 
+            // sonuç2
+            // 
+            this.sonuç2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sonuç2.Location = new System.Drawing.Point(115, 321);
+            this.sonuç2.Name = "sonuç2";
+            this.sonuç2.Size = new System.Drawing.Size(37, 23);
+            this.sonuç2.TabIndex = 20;
+            // 
+            // sonuç4
+            // 
+            this.sonuç4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sonuç4.Location = new System.Drawing.Point(115, 370);
+            this.sonuç4.Name = "sonuç4";
+            this.sonuç4.Size = new System.Drawing.Size(37, 23);
+            this.sonuç4.TabIndex = 19;
+            // 
+            // izbtn
+            // 
+            this.izbtn.Location = new System.Drawing.Point(227, 243);
+            this.izbtn.Name = "izbtn";
+            this.izbtn.Size = new System.Drawing.Size(82, 23);
+            this.izbtn.TabIndex = 16;
+            this.izbtn.Text = "MATRİS İZİ";
+            this.izbtn.UseVisualStyleBackColor = true;
+            this.izbtn.Click += new System.EventHandler(this.izbtn_Click);
+            // 
+            // transpozbtn
+            // 
+            this.transpozbtn.Location = new System.Drawing.Point(86, 243);
+            this.transpozbtn.Name = "transpozbtn";
+            this.transpozbtn.Size = new System.Drawing.Size(82, 23);
+            this.transpozbtn.TabIndex = 15;
+            this.transpozbtn.Text = "TRANSPOZU";
+            this.transpozbtn.UseVisualStyleBackColor = true;
+            this.transpozbtn.Click += new System.EventHandler(this.transpozbtn_Click);
+            // 
+            // tersibtn
+            // 
+            this.tersibtn.Location = new System.Drawing.Point(295, 196);
+            this.tersibtn.Name = "tersibtn";
+            this.tersibtn.Size = new System.Drawing.Size(82, 23);
+            this.tersibtn.TabIndex = 14;
+            this.tersibtn.Text = "TERS ALMA";
+            this.tersibtn.UseVisualStyleBackColor = true;
+            this.tersibtn.Click += new System.EventHandler(this.tersibtn_Click);
+            // 
+            // çarpbtn
+            // 
+            this.çarpbtn.Location = new System.Drawing.Point(156, 196);
+            this.çarpbtn.Name = "çarpbtn";
+            this.çarpbtn.Size = new System.Drawing.Size(82, 23);
+            this.çarpbtn.TabIndex = 13;
+            this.çarpbtn.Text = "ÇARPMA";
+            this.çarpbtn.UseVisualStyleBackColor = true;
+            this.çarpbtn.Click += new System.EventHandler(this.çarpbtn_Click);
+            // 
+            // toplambtn
+            // 
+            this.toplambtn.Location = new System.Drawing.Point(27, 196);
+            this.toplambtn.Name = "toplambtn";
+            this.toplambtn.Size = new System.Drawing.Size(82, 23);
+            this.toplambtn.TabIndex = 12;
+            this.toplambtn.Text = "TOPLAMA";
+            this.toplambtn.UseVisualStyleBackColor = true;
+            this.toplambtn.Click += new System.EventHandler(this.toplambtn_Click);
+            // 
+            // matris12
+            // 
+            this.matris12.Location = new System.Drawing.Point(115, 67);
+            this.matris12.Name = "matris12";
+            this.matris12.Size = new System.Drawing.Size(37, 23);
+            this.matris12.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(450, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 421);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İŞLEMLER";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(9, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(215, 399);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(678, 421);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox matris13;
+        private System.Windows.Forms.TextBox matris21;
+        private System.Windows.Forms.TextBox matris23;
+        private System.Windows.Forms.TextBox matris24;
+        private System.Windows.Forms.TextBox matris22;
+        private System.Windows.Forms.TextBox matris11;
+        private System.Windows.Forms.TextBox matris14;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label sonuç5;
+        private System.Windows.Forms.Label sonuç3;
+        private System.Windows.Forms.Label sonuç2;
+        private System.Windows.Forms.Label sonuç4;
+        private System.Windows.Forms.Button transpozbtn;
+        private System.Windows.Forms.Button tersibtn;
+        private System.Windows.Forms.Button çarpbtn;
+        private System.Windows.Forms.Button toplambtn;
+        private System.Windows.Forms.TextBox matris12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label sonuç1;
+        private System.Windows.Forms.Button izbtn;
+        private System.Windows.Forms.Button görüntübtn;
+    }
+}
+
---
+++ ndpödev2soru2/Form1.cs
@@ -0,0 +1,195 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2020-2021 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........:ÖDEV2_SORU2
+**				ÖĞRENCİ ADI............:MEHMET ARI
+**				ÖĞRENCİ NUMARASI.......:B201210086
+**              DERSİN ALINDIĞI GRUP...:1. ÖĞRETİM B GRUBU
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ndpödev2soru2
+{
+    public partial class Form1 : Form
+    {
+        string dosyayolu;
+        public Form1()
+        {
+            InitializeComponent();
+            dosyayolu = "işlemler.txt";    
+        }
+
+        private void çarpbtn_Click(object sender, EventArgs e)
+        {
+            //matris1, matris2, sonuçmatris adında 3 tane iki boyutlu dizi oluşturdum.
+            double[,] matris1 = new double[2, 2];
+            matris1[0, 0] = double.Parse(matris11.Text);
+            matris1[1, 0] = double.Parse(matris12.Text);
+            matris1[0, 1] = double.Parse(matris13.Text);
+            matris1[1, 1] = double.Parse(matris14.Text);
+
+            double[,] matris2 = new double[2, 2];
+            matris2[0, 0] = double.Parse(matris21.Text);
+            matris2[1, 0] = double.Parse(matris22.Text);
+            matris2[0, 1] = double.Parse(matris23.Text);
+            matris2[1, 1] = double.Parse(matris24.Text);
+            //İşlemler yaptırılır.
+            double[,] sonuçmatris = new double[2, 2];
+            sonuçmatris[0, 0] = matris1[0, 0] * matris2[0, 0] + matris1[1, 0] * matris2[0, 1];
+            sonuçmatris[1, 0] = matris1[0, 0] * matris2[1, 0] + matris1[1, 0] * matris2[1, 1];
+            sonuçmatris[0, 1] = matris1[0, 1] * matris2[0, 0] + matris1[1, 1] * matris2[0, 1];
+            sonuçmatris[1, 1] = matris1[0, 1] * matris2[1, 0] + matris1[1, 1] * matris2[1, 1];
+            //text içerisine yazdırılır.
+            sonuç1.Text = sonuçmatris[0, 0].ToString();
+            sonuç2.Text = sonuçmatris[1, 0].ToString();
+            sonuç3.Text = sonuçmatris[0, 1].ToString();
+            sonuç4.Text = sonuçmatris[1, 1].ToString();
+            //Dosya yoksa Oluşturur.
+            if (!File.Exists(dosyayolu))
+            {
+                File.Create(dosyayolu);
+
+            }//Veriler dosyaya yazdırılır.
+            File.WriteAllText(dosyayolu, matris11.Text + " " + matris12.Text + "\n" + matris13.Text + " " + matris14.Text + "\n" + "x" + "\n" + matris21.Text + " " + matris22.Text + "\n" + matris23.Text + " " + matris24.Text + "\n" + "=" + "\n" + sonuç1.Text + " " + sonuç2.Text + "\n" + sonuç3.Text + " " + sonuç4.Text);
+        }
+
+        private void toplambtn_Click(object sender, EventArgs e)
+        {
+            //matris1, matris2, sonuçmatris adında 3 tane iki boyutlu dizi oluşturdum.
+            double[,] matris1 = new double[2, 2];
+            matris1[0, 0] = double.Parse(matris11.Text);
+            matris1[1, 0] = double.Parse(matris12.Text);
+            matris1[0, 1] = double.Parse(matris13.Text);
+            matris1[1, 1] = double.Parse(matris14.Text);
+
+            double[,] matris2 = new double[2, 2];
+            matris2[0, 0] = double.Parse(matris21.Text);
+            matris2[1, 0] = double.Parse(matris22.Text);
+            matris2[0, 1] = double.Parse(matris23.Text);
+            matris2[1, 1] = double.Parse(matris24.Text);
+            //İşlemler yaptırılır.
+            double[,] sonuçmatris = new double[2, 2];
+            sonuçmatris[0, 0] = matris1[0, 0] + matris2[0, 0];
+            sonuçmatris[1, 0] = matris1[1, 0] + matris2[1, 0];
+            sonuçmatris[0, 1] = matris1[0, 1] + matris2[0, 1];
+            sonuçmatris[1, 1] = matris1[1, 1] + matris2[1, 1];
+            //text içerisine yazdırılır.
+            sonuç1.Text = sonuçmatris[0, 0].ToString();
+            sonuç2.Text = sonuçmatris[1, 0].ToString();
+            sonuç3.Text = sonuçmatris[0, 1].ToString();
+            sonuç4.Text = sonuçmatris[1, 1].ToString();
+            //Dosya yoksa Oluşturur.
+            if (!File.Exists(dosyayolu))
+            {
+                File.Create(dosyayolu);
+
+            }//Veriler dosyaya yazdırılır.
+            File.WriteAllText(dosyayolu, matris11.Text + " " + matris12.Text + "\n" + matris13.Text + " " + matris14.Text + "\n" + "+" + "\n" + matris21.Text + " " + matris22.Text + "\n" + matris23.Text + " " + matris24.Text + "\n" + "=" + "\n" + sonuç1.Text + " " + sonuç2.Text + "\n" + sonuç3.Text + " " + sonuç4.Text);
+        }
+
+        private void tersibtn_Click(object sender, EventArgs e)
+        {
+            //matris1 ve sonuçmatris adında 2 tane iki boyutlu dizi oluşturdum.
+            double[,] matris1 = new double[2, 2];
+            matris1[0, 0] = double.Parse(matris11.Text);
+            matris1[1, 0] = double.Parse(matris12.Text);
+            matris1[0, 1] = double.Parse(matris13.Text);
+            matris1[1, 1] = double.Parse(matris14.Text);
+
+            double determinant = matris1[0, 0] * matris1[1, 1] - matris1[1, 0] * matris1[0, 1];
+            //İşlemler yaptırılır.
+            double[,] sonuçmatris = new double[2, 2];
+            sonuçmatris[0, 0] = (1 / determinant) * matris1[1, 1];
+            sonuçmatris[1, 0] = (1 / determinant) * -(matris1[1, 0]);
+            sonuçmatris[0, 1] = (1 / determinant) * -(matris1[0, 1]);
+            sonuçmatris[1, 1] = (1 / determinant) * matris1[0, 0];
+            //text içerisine yazdırılır.
+            sonuç1.Text = sonuçmatris[0, 0].ToString();
+            sonuç2.Text = sonuçmatris[1, 0].ToString();
+            sonuç3.Text = sonuçmatris[0, 1].ToString();
+            sonuç4.Text = sonuçmatris[1, 1].ToString();
+            //Dosya yoksa Oluşturur.
+            if (!File.Exists(dosyayolu))
+            {
+                File.Create(dosyayolu);
+
+            }//Veriler dosyaya yazdırılır.
+            File.WriteAllText(dosyayolu, "1/" + determinant + "\n" + " x " + "\n" + matris11.Text + " " + matris12.Text + "\n" + matris13.Text + " " + matris14.Text + "\n" + "=" + "\n" + sonuç1.Text + " " + sonuç2.Text + "\n" + sonuç3.Text + " " + sonuç4.Text);
+        }
+
+        private void transpozbtn_Click(object sender, EventArgs e)
+        {
+            //matris1 ve sonuçmatris adında 2 tane iki boyutlu dizi oluşturdum.
+            double[,] matris1 = new double[2, 2];
+            matris1[0, 0] = double.Parse(matris11.Text);
+            matris1[1, 0] = double.Parse(matris12.Text);
+            matris1[0, 1] = double.Parse(matris13.Text);
+            matris1[1, 1] = double.Parse(matris14.Text);
+            //text içerisine yazdırılır.
+            sonuç1.Text = matris1[0, 0].ToString();
+            sonuç2.Text = matris1[0, 1].ToString();
+            sonuç3.Text = matris1[1, 0].ToString();
+            sonuç4.Text = matris1[1, 1].ToString();
+            //Dosya yoksa Oluşturur.
+            if (!File.Exists(dosyayolu))
+            {
+                File.Create(dosyayolu);
+
+            }//Veriler dosyaya yazdırılır.
+            File.WriteAllText(dosyayolu, matris11.Text + " " + matris12.Text + "\n" + matris13.Text + " " + matris14.Text + "\n" + "MATRİSİN TRANZPOZU" + "\n" + sonuç1.Text + " " + sonuç2.Text + "\n" + sonuç3.Text + " " + sonuç4.Text);
+        }
+
+        private void izbtn_Click(object sender, EventArgs e)
+        {
+            //matris1 adında iki boyutlu dizi oluşturdum.
+            double[,] matris1 = new double[2, 2];
+            matris1[0, 0] = double.Parse(matris11.Text);
+            matris1[1, 0] = double.Parse(matris12.Text);
+            matris1[0, 1] = double.Parse(matris13.Text);
+            matris1[1, 1] = double.Parse(matris14.Text);
+            //İşlemler yapılır ve text içerisine yazılır.
+            sonuç5.Text = (matris1[0, 0] + matris1[1, 1]).ToString();
+
+            //Dosya yoksa oluşturur.
+            if (!File.Exists(dosyayolu))
+            {
+                File.Create(dosyayolu);
+
+            }//Veriler dosyaya yazdırılır.
+            File.WriteAllText(dosyayolu, matris11.Text + " + " + matris14.Text + " = " + sonuç5.Text);
+        }
+
+        private void görüntübtn_Click(object sender, EventArgs e)
+        {
+            dosyayolu = "işlemler.txt";
+            //Dosya okuma modunda açılır.
+            FileStream fileStream = new FileStream(dosyayolu, FileMode.OpenOrCreate, FileAccess.Read);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (true)//Dosyanın içindeki veriler okutulur ve richtexbox1 içerisine yazılır.
+                {
+                    string satir = reader.ReadLine();
+                    richTextBox1.Text += satir + "\n";
+                    if (satir == null) //Eğer dosya içinde boş bir satıra gelinirse döngüden çıkılır.
+                        break;
+                }
+                reader.Close();
+            }
+            fileStream.Close();
+        }
+    } 
+}
